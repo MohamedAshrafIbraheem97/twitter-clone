b11fffc8b05399e9bd4f58718f8bb705392924e7
Mohamed Ashraf Ibraheem <mohamedashrafibraheem@gmail.com>
2023-01-07 05:59:10 +0200

    added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { ContentComponent } from './content/content.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    ContentComponent
+    ContentComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
