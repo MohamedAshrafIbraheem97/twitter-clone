0e495ffeb2deded50c7cfd3a3c6c911d2416dd4b
Mohamed Ashraf Ibraheem <mohamedashrafibraheem@gmail.com>
2023-01-12 02:57:36 +0200

    listed all tweet based on creation date

--- src/app/tweets/list-tweets/list-tweets.component.ts
+++ src/app/tweets/list-tweets/list-tweets.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Tweet } from '../models/tweet.model';
+import { TweetService } from '../tweet.service';
 
 @Component({
   selector: 'app-list-tweets',
   templateUrl: './list-tweets.component.html',
-  styleUrls: ['./list-tweets.component.sass']
+  styleUrls: ['./list-tweets.component.sass'],
 })
-export class ListTweetsComponent {
+export class ListTweetsComponent implements OnInit, OnDestroy {
+  tweets: Tweet[] = [];
+  subscription: Subscription;
 
+  constructor(private tweetService: TweetService) {}
+
+  ngOnInit(): void {
+    this.tweets = this.tweetService.getTweets();
+
+    this.subscription = this.tweetService.tweetListChanged.subscribe(
+      (tweets: Tweet[]) => {
+        this.tweets = tweets;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/tweets/list-tweets/tweet/tweet.component.ts
+++ src/app/tweets/list-tweets/tweet/tweet.component.ts
@@ -1,13 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { IconName } from 'src/app/shared-components/icon/IconName';
+import { Tweet } from '../../models/tweet.model';
 
 @Component({
   selector: 'app-tweet',
   templateUrl: './tweet.component.html',
   styleUrls: ['./tweet.component.sass'],
 })
-export class TweetComponent {
+export class TweetComponent implements OnInit {
+  @Input('tweetData') tweet: Tweet;
+
   likeIcon: IconName = IconName.LIKE;
   replyIcon: IconName = IconName.REPLY;
   retweetIcon: IconName = IconName.RETWEET;
+
+  // prettier-ignore
+  MonthNames: string[] = ['Jan','Feb','Mar','Apr','May','Jun',
+                          'Jul','Aug','Sep','Oct','Nov','Dec',];
+
+  ngOnInit(): void {
+    // console.log(date.getHours());
+    // console.log(new Date().getMinutes() - date.getMinutes());
+  }
+
+  handlingDate() {
+    let tweetDate = this.tweet.creationDate;
+    let currentDate = new Date();
+    let datePreview = '';
+
+    if (currentDate.getFullYear() === tweetDate.getFullYear()) {
+      if (currentDate.getMonth() === tweetDate.getMonth()) {
+        if (currentDate.getDate() === tweetDate.getDate()) {
+          if (currentDate.getHours() === tweetDate.getHours()) {
+            datePreview =
+              currentDate.getMinutes() - tweetDate.getMinutes() + 'm';
+          } else {
+            datePreview = currentDate.getHours() - tweetDate.getHours() + 'H';
+          }
+        } else {
+          datePreview =
+            this.MonthNames[tweetDate.getMonth()] + ' ' + tweetDate.getDate();
+        }
+      }
+    } else {
+      datePreview =
+        this.MonthNames[tweetDate.getMonth()] +
+        ' ' +
+        tweetDate.getDate() +
+        ', ' +
+        tweetDate.getFullYear();
+    }
+
+    return datePreview;
+  }
 }
